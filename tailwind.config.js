652955aaed32b5fb9bf2e00e54f0151177df34d4
taufanbasri <taufanprasetyobasri@gmail.com>
2022-04-01 15:51:36 +0700

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'lexend': ['Lexend Deca', 'sans-serif']
+      },
+      colors: {
+        'dark-blue': 'hsl(233, 47%, 7%)',
+        'desaturated-blue': 'hsl(244, 38%, 16%)',
+        'soft-violet': 'hsl(277, 64%, 61%)',
+        'white-trans-75': 'hsla(0, 0%, 100%, 0.75)',
+        'white-trans-60': 'hsla(0, 0%, 100%, 0.6)'
+      }
+    },
+  },
+  plugins: [],
+}
